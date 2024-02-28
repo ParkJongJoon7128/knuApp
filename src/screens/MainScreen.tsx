74e602fd87f34c7af27303c30bce3686a50bd418
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-02-29 00:08:01 +0900

    사용자 정보 가져오기 기능 구현 완료

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,11 +1,20 @@
 import { firebase } from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 
 const MainScreen = () => {
   // Logic
   const navigation = useNavigation();
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  const onAuthStateChanged = user => {
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  };
 
   const LogOut = () => {
     return firebase
@@ -19,6 +28,15 @@ const MainScreen = () => {
         console.log(err.message);
       });
   };
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) {
+    return null;
+  }
 
   // Views
   return (
