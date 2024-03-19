940ce43bebc76b0a47151d25a93d14b497401bc4
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-03-19 17:04:37 +0900

    카카오 검색 기능 구현 완료

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,4 +1,4 @@
-import { API_URL, NAVER_MAP_API_KEY_ID, NAVER_MAP_API_SECRET } from '@env';
+import { API_URL, KAKAO_REST_API_KEY } from '@env';
 import database from '@react-native-firebase/database';
 import { firebase } from '@react-native-firebase/firestore';
 import { useNavigation } from '@react-navigation/native';
@@ -9,10 +9,10 @@ import {
   SafeAreaView,
   StyleSheet,
   TextInput,
-  View,
+  View
 } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
-import NaverMapView, { Marker } from 'react-native-nmap';
+import NaverMapView from 'react-native-nmap';
 
 const MainScreen = () => {
   // Logic
@@ -31,77 +31,158 @@ const MainScreen = () => {
     }
   };
 
-  const searchAddress = async (address: string) => {
+  const searchAddress = async (addr: string) => {
     try {
       await axios
         .get(API_URL, {
+          // headers: {
+          //   'X-Naver-Client-Id': NAVER_MAP_API_KEY_ID,
+          //   'X-Naver-Client-Secret': NAVER_MAP_API_SECRET,
+          // },
+          // params: {
+          //   query: addr,
+          //   display: 5,
+          //   start: 1,
+          //   sort: 'sim',
+          // },
+          // headers: {
+          //   'X-NCP-APIGW-API-KEY-ID': NAVER_MAP_API_KEY_ID,
+          //   'X-NCP-APIGW-API-KEY': NAVER_MAP_API_SECRET,
+          // },
+          // params: {
+          //   query: address,
+          // },
           headers: {
-            'X-NCP-APIGW-API-KEY-ID': NAVER_MAP_API_KEY_ID,
-            'X-NCP-APIGW-API-KEY': NAVER_MAP_API_SECRET,
-          },
+            "Authorization": KAKAO_REST_API_KEY,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+          }, 
           params: {
-            query: address,
-          },
+            query: addr
+          }
         })
         .then(result => {
-          // console.log(result.data);
-          const latitude = parseFloat(result.data.addresses[0].y);
-          const longitude = parseFloat(result.data.addresses[0].x);
-          setLocation({latitude, longitude});
+          console.log("검색 결과: ", result.data.documents);
+          // const latitude = parseFloat(result.data.items[0].mapy);
+          // const longitude = parseFloat(result.data.items[0].mapx);
+          // const category = result.data.items[0].category;
+          // addLocation(user, {latitude, longitude}, category);
+          
+          // const latitude = parseFloat(result.data.addresses[0].y);
+          // const longitude = parseFloat(result.data.addresses[0].x);
+          // const roadAddress = result.data.addresses[0].roadAddress;
+          // addLocation(user, {latitude, longitude}, roadAddress);
+          // mapRef.current.animateToCoordinate({latitude, longitude});
+
+          const latitude = parseFloat(result.data.documents[0].y);
+          const longitude = parseFloat(result.data.documents[0].x);
+          const category = result.data.documents[0].category_group_name;
+          const placeName = result.data.documents[0].place_name;
+          const number = result.data.documents[0].phone;
+
+          addLocation(user, {latitude, longitude}, category, placeName, number)
           mapRef.current.animateToCoordinate({latitude, longitude});
-          addItem(user, {latitude, longitude});
         })
         .catch(err => {
-          console.log(err);
+          console.log('주소 검색 실패: ', err);
         });
     } catch (error) {
-      console.log(error);
+      console.log('주소 검색 에러: ', error);
     }
   };
 
-  const addItem = (
+  const addLocation = async (
     user: any,
     location: {latitude: number; longitude: number},
+    category: any,
+    placeName: any,
+    number: any,
   ) => {
     try {
-      // firestore().collection('profile')
-      //   .doc(user.uid)
-      //   .set({user: user.displayName, text: location})
-      //   .then(res => {
-      //     console.log('res: ', res);
-      //   })
-      //   .catch(err => {
-      //     console.log('err: ', err);
-      //   });
-      database()
+      await database()
         .ref('locations/')
         .child(user.uid)
-        .set({
-          address,
-          location,
-        })
+        .push({category, location, placeName, number})
+        .once('value')
         .then(res => {
-          console.log('res: ', res);
+          console.log('데이터 추가: ', res);
         })
-        .catch(res => {
-          console.log('res: ', res);
+        .catch(err => {
+          console.log('데이터 추가 실패: ', err);
         });
     } catch (error) {
-      console.log('error: ', error);
+      console.log('데이터 추가 에러: ', error);
     }
   };
+
+  // const searchAddress = async (address: string) => {
+  //   try {
+  //     await axios
+  //       .get(API_URL, {
+  //         headers: {
+  //           // 'X-NCP-APIGW-API-KEY-ID': NAVER_MAP_API_KEY_ID,
+  //           // 'X-NCP-APIGW-API-KEY': NAVER_MAP_API_SECRET,
+  //           "X-Naver-Client-Id": NAVER_MAP_API_KEY_ID,
+  //           "X-Naver-Client-Secret": NAVER_MAP_API_SECRET
+  //         },
+  //         params: {
+  //           query: address,
+  //           display: 100,
+  //           start: 1,
+  //           sort: "sim"
+  //         },
+  //       })
+  //       .then(result => {
+  //         console.log("주소 검색: ", result.data.items[0].mapy);
+  //         console.log("주소 검색: ", result.data.items[0].mapx);
+  //         const latitude = parseInt(result.data.items[0].mapy);
+  //         const longitude = parseInt(result.data.items[0].mapx);
+  //         setLocation({latitude, longitude});
+  //         mapRef.current.animateToCoordinate({latitude, longitude});
+  //         addLocation(user, {latitude, longitude});
+  //       })
+  //       .catch(err => {
+  //         console.log("주소 검색 에러: ", err);
+  //       });
+  //   } catch (error) {
+  //     console.log("주소 검색 통신 에러: ", error);
+  //   }
+  // };
+
+  // const addLocation = (
+  //   user: any,
+  //   location: {latitude: number; longitude: number},
+  // ) => {
+  //   try {
+  //     database()
+  //       .ref('locations/')
+  //       .on("value")
+  //       .child(user.uid)
+  //       .update({
+  //         address,
+  //         location,
+  //       })
+  //       .then(res => {
+  //         console.log('주소 추가: ', res);
+  //       })
+  //       .catch(res => {
+  //         console.log('주소 추가 에러: ', res);
+  //       });
+  //   } catch (error) {
+  //     console.log('주소 추가 통신 에러: ', error);
+  //   }
+  // };
 
   useEffect(() => {
     Geolocation.getCurrentPosition(
       position => {
-        // console.log('현재위치: ', position.coords);
+        console.log('현재위치: ', position.coords);
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
         setLocation({latitude, longitude});
         mapRef.current.animateToCoordinate({latitude, longitude});
       },
       error => {
-        console.log(error);
+        console.log('현재 위치 정보 받기 에러: ', error);
       },
       {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
     );
@@ -121,14 +202,7 @@ const MainScreen = () => {
           style={styles.mapView}
           ref={mapRef}
           showsMyLocationButton={true}
-          center={{...location, zoom: 16}}>
-          <Marker
-            ref={mapRef}
-            coordinate={location}
-            pinColor="blue"
-            onClick={() => console.log('onClick! p1')}
-          />
-        </NaverMapView>
+          center={{...location, zoom: 16}}></NaverMapView>
       </View>
 
       {/* TextInput을 절대 위치로 지정하여 Map 위에 위치하도록 함 */}
