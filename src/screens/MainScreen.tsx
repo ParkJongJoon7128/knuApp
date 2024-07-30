a5f00da3c32a9e594aaa965e500fb0ff38ee9a78
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-07-30 23:56:40 +0900

    bottomsheet 디자인 작업중

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -5,7 +5,14 @@ import {
   BottomSheetView,
 } from '@gorhom/bottom-sheet';
 import React, { useCallback, useEffect, useMemo, useRef } from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import NaverMapView, { Marker } from 'react-native-nmap';
 import { data } from '../data/location';
 
@@ -19,6 +26,8 @@ const MainScreen = () => {
     latitude: 37.2700052079,
     longitude: 127.12624831737,
   };
+
+  const screenWidth = Dimensions.get('window').width;
 
   const handlePresentModalPress = useCallback(() => {
     bottomSheetModalRef.current?.present();
@@ -44,16 +53,59 @@ const MainScreen = () => {
       <View
         style={{
           flex: 1,
-          paddingHorizontal: 15,
-          flexDirection: 'column',
+          width: '100%',
           justifyContent: 'center',
           alignItems: 'center',
+          flexDirection: 'column',
+          paddingHorizontal: 15,
         }}>
         <Image
           source={{uri: item.imageUrl?.toString()}}
-          style={{width: '100%', height: 200}}
+          style={{width: '100%', height: 250, borderRadius: 15}}
+          resizeMode="center"
         />
-        <Text style={{alignSelf: 'flex-start'}}>{item.name}</Text>
+
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%',
+            alignSelf: 'flex-start',
+            marginTop: 10,
+          }}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{fontSize: 20, fontWeight: 'bold', marginRight: 5}}>
+              {item.name}
+            </Text>
+
+            <Text style={{color: 'gray'}}>{item.category}</Text>
+          </View>
+
+          <View>
+            <TouchableOpacity>
+              <Image
+                source={require('../assets/images/unclicked_star.png')}
+                style={{width: 20, height: 20}}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View
+          style={{
+            width: '100%',
+            borderWidth: 0.5,
+            borderColor: 'lightgray',
+            marginVertical: 20,
+          }}
+        />
       </View>
     );
   };
@@ -96,6 +148,52 @@ const MainScreen = () => {
           onChange={handleSheetChanges}
           backdropComponent={handleSheetBackdrop}>
           <BottomSheetView>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                marginVertical: 15,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderWidth: 0.5,
+                borderColor: 'lightgray',
+                borderRadius: 15,
+                alignSelf: 'center',
+                paddingHorizontal: 10,
+                paddingVertical: 3,
+              }}>
+              <Image
+                source={require('../assets/images/pin.png')}
+                style={{width: 16, height: 16, marginRight: 3}}
+                resizeMode="contain"
+                tintColor="#007AFF"
+              />
+              <Text style={{fontWeight: '700', fontSize: 16}}>
+                강남대학교 맛집 리스트
+              </Text>
+            </View>
+
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingHorizontal: 35,
+                marginBottom: 15,
+              }}>
+              <Text style={{fontWeight: '700', fontSize: 20, marginRight: 5}}>
+                주변
+              </Text>
+              <View
+                style={{
+                  width: '100%',
+                  borderWidth: 0.5,
+                  borderColor: 'lightgray',
+                }}
+              />
+            </View>
+
             <FlatList
               data={data}
               renderItem={locationRenderItem}
